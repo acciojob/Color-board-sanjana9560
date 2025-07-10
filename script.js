6dc0528bd0a90d97e042dc1de5429975c6501507
sanjana9560 <121418058+sanjana9560@users.noreply.github.com>
2025-07-10 07:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+
+const container = document.getElementById("board");
+const SQUARES = 800;
+
+// Generate random color
+function getRandomColor() {
+  const chars = "0123456789ABCDEF";
+  let color = "#";
+  for(let i = 0; i < 6; i++) {
+    color += chars[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+for(let i = 0; i < SQUARES; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    const color = getRandomColor();
+    square.style.backgroundColor = color;
+
+   
+    setTimeout(() => {
+      square.style.backgroundColor = "#1d1d1d";
+    }, 1000);
+  });
+
+  container.appendChild(square);
+}
